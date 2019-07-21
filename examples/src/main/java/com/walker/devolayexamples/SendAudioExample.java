2641fe6bd2d52efc53a007dd34e05ab4b129a2f1
Walker Knapp <walkerknapp3@gmail.com>
2019-07-21 13:40:24 -0400

    Added Audio Sending example

---
+++ examples/src/main/java/com/walker/devolayexamples/SendAudioExample.java
@@ -0,0 +1,60 @@
+package com.walker.devolayexamples;
+
+import com.walker.devolay.DevolayAudioFrame;
+import com.walker.devolay.DevolaySender;
+
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+
+
+/**
+ * Adapted from NDIlib_Send_Audio.cpp
+ */
+public class SendAudioExample {
+    public static void main(String[] args) {
+        DevolaySender sender = new DevolaySender("Devolay Example Audio", null, false, true);
+
+        final int sampleRate = 48000;
+        final int channelCount = 4;
+        // One tenth of a second of audio
+        final int sampleCount = sampleRate / 10;
+
+        final ByteBuffer data = ByteBuffer.allocateDirect((sampleCount * channelCount * Float.SIZE) / Byte.SIZE);
+        final FloatBuffer floatData = data.asFloatBuffer();
+
+        DevolayAudioFrame audioFrame = new DevolayAudioFrame();
+        audioFrame.setSampleRate(sampleRate);
+        audioFrame.setChannels(channelCount);
+        audioFrame.setSamples(sampleCount);
+        audioFrame.setData(data);
+        audioFrame.setChannelStride((sampleCount * Float.SIZE) / Byte.SIZE);
+
+        // Generate 10 seconds of a 4 different sine wave frequencies
+        final float c6NoteFreq = 1046.50f;
+        final float e6NoteFreq = 1318.51f;
+        final float g6NoteFreq = 1567.98f;
+        final float c7NoteFreq = 2093.00f;
+        final float[] frequencyPerChannel = {c6NoteFreq, e6NoteFreq, g6NoteFreq, c7NoteFreq};
+        // Ten seconds of samples
+        final int totalSamplesToSend = sampleRate * 10;
+
+        for(int i = 0; i < totalSamplesToSend; i += sampleCount) {
+            floatData.position(0);
+
+            for(int ch = 0; ch < channelCount; ch++) {
+                for(int sample = 0; sample < sampleCount; sample++) {
+
+                    floatData.put((float) Math.sin(sample * 2f * Math.PI * frequencyPerChannel[ch]));
+
+                }
+            }
+
+            floatData.flip();
+            sender.sendAudioFrame(audioFrame);
+        }
+
+        // Destroy the references to each. Not necessary, but can free up the memory faster than Java's GC by itself
+        audioFrame.close();
+        sender.close();
+    }
+}

06958166a66db6e4cbea68137dd6583d2efd9fb1
Walker Knapp <walkerknapp3@gmail.com>
2020-04-26 00:53:05 -0400

    Fall back to gulrak/filesystem when c++17 N/A

--- devolay-natives/src/main/cpp/devolay.cpp
+++ devolay-natives/src/main/cpp/devolay.cpp
@@ -12,37 +12,20 @@
 #endif
 
 #if defined(__cpp_lib_filesystem)
-    #define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 0
-#elif defined(__cpp_lib_experimental_filesystem)
-    #define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
-#elif !defined(__has_include)
-    #define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
-#elif __has_include(<filesystem>)
-    #ifdef _MSC_VER
-        #if __has_include(<yvals_core.h>)
-            #include <yvals_core.h>
-            #if defined(_HAS_CXX17) && _HAS_CXX17
-               #define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 0
-            #endif
-        #endif
-        #ifndef INCLUDE_STD_FILESYSTEM_EXPERIMENTAL
-            #define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
-        #endif
-    #else // #ifdef _MSC_VER
-        #define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 0
-    #endif
-#elif __has_include(<experimental/filesystem>)
-    #define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
-#else
-    #error "C++17 support no found. Please update your compiler to the latest version."
-#endif
-
-#if INCLUDE_STD_FILESYSTEM_EXPERIMENTAL
-    #include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
-#else
     #include <filesystem>
-namespace fs = std::filesystem;
+    namespace fs = std::filesystem;
+#elif defined(__cpp_lib_experimental_filesystem) || !defined(__has_include)
+    #include <experimental/filesystem>
+    namespace fs = std::experimental::filesystem;
+#elif __has_include(<filesystem>)
+    #include <filesystem>
+    namespace fs = std::filesystem;
+#elif __has_include(<experimental/filesystem>)
+    #include <experimental/filesystem>
+    namespace fs = std::experimental::filesystem;
+#else
+    #include <ghc/filesystem.hpp>
+    namespace fs = ghc::filesystem;
 #endif
 
 #include "../headers/com_walker_devolay_Devolay.h"

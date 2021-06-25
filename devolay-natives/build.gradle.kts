06002b21d0c5a1739f0e5962229f09be1c98b7d3
Walker Knapp <walkerknapp3@gmail.com>
2021-06-25 14:40:42 -0400

    Only add -llog on android builds

--- devolay-natives/build.gradle.kts
+++ devolay-natives/build.gradle.kts
@@ -45,19 +45,29 @@ tasks.withType(CppCompile::class).configureEach {
     compilerArgs.addAll(toolChain.map { toolChain ->
         when (toolChain) {
             is VisualCpp -> listOf("/std:c++11")
-            is GccCompatibleToolChain -> listOf("-lstdc++", "-std=c++11", "-static-libgcc", "-static-libstdc++", "-ldl", "-llog")
+            is GccCompatibleToolChain -> listOf("-lstdc++", "-std=c++11", "-static-libgcc", "-static-libstdc++", "-ldl")
             else -> listOf()
         }
     })
+
+    // A little bit of a weird workaround because we can't resolve targetMachine here
+    if (name.toLowerCase().contains("android")) {
+        compilerArgs.add("-llog")
+    }
 }
 
 tasks.withType(LinkSharedLibrary::class).configureEach {
     linkerArgs.addAll(toolChain.map { toolChain ->
         when (toolChain) {
-            is GccCompatibleToolChain -> listOf("-shared", "-static-libgcc", "-static-libstdc++", "-ldl", "-llog")
+            is GccCompatibleToolChain -> listOf("-shared", "-static-libgcc", "-static-libstdc++", "-ldl")
             else -> listOf()
         }
     })
+
+    // A little bit of a weird workaround because we can't resolve targetMachine here
+    if (name.toLowerCase().contains("android")) {
+        linkerArgs.add("-llog")
+    }
 }
 
 library {

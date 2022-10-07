bef3959d6e049b3b2461a40c25b4063e0c487e6a
nocarryr <matt@nomadic-recording.com>
2022-10-07 15:16:16 -0500

    Correct pointer references and method calls

--- devolay-natives/src/main/cpp/receiver.cpp
+++ devolay-natives/src/main/cpp/receiver.cpp
@@ -35,15 +35,15 @@ JNIEXPORT jlong JNICALL Java_me_walkerknapp_devolay_DevolayReceiver_receiveCreat
 }
 
 JNIEXPORT void JNICALL Java_me_walkerknapp_devolay_DevolayReceiver_receiveDestroy(JNIEnv *env, jclass jClazz, jlong pReceiver) {
-    delete reinterpret_cast<NDIlib_recv_instance_t *>(pReceiver);
+    delete reinterpret_cast<NDIlib_recv_instance_t>(pReceiver);
 }
 
 JNIEXPORT void JNICALL Java_me_walkerknapp_devolay_DevolayReceiver_receiveConnect(JNIEnv *env, jclass jClazz, jlong pReceiver, jlong pSource) {
-    getNDILib()->recv_connect(reinterpret_cast<NDIlib_recv_instance_t *>(pReceiver), reinterpret_cast<NDIlib_source_t *>(pSource));
+    getNDILib()->recv_connect(reinterpret_cast<NDIlib_recv_instance_t>(pReceiver), reinterpret_cast<NDIlib_source_t *>(pSource));
 }
 
 JNIEXPORT jint JNICALL Java_me_walkerknapp_devolay_DevolayReceiver_receiveCaptureV2(JNIEnv *env, jclass jClazz, jlong pReceiver, jlong pVideoFrame, jlong pAudioFrame, jlong pMetadataFrame, jint timeout) {
-    return getNDILib()->recv_capture_v2(reinterpret_cast<NDIlib_recv_instance_t *>(pReceiver),
+    return getNDILib()->recv_capture_v2(reinterpret_cast<NDIlib_recv_instance_t>(pReceiver),
                                   reinterpret_cast<NDIlib_video_frame_v2_t *>(pVideoFrame),
                                   reinterpret_cast<NDIlib_audio_frame_v2_t *>(pAudioFrame),
                                   reinterpret_cast<NDIlib_metadata_frame_t *>(pMetadataFrame),
@@ -51,47 +51,47 @@ JNIEXPORT jint JNICALL Java_me_walkerknapp_devolay_DevolayReceiver_receiveCaptur
 }
 
 JNIEXPORT void JNICALL Java_me_walkerknapp_devolay_DevolayReceiver_freeVideoV2(JNIEnv *env, jclass jClazz, jlong pReceiver, jlong pVideoFrame) {
-    getNDILib()->recv_free_video_v2(reinterpret_cast<NDIlib_recv_instance_t *>(pReceiver),
+    getNDILib()->recv_free_video_v2(reinterpret_cast<NDIlib_recv_instance_t>(pReceiver),
                               reinterpret_cast<NDIlib_video_frame_v2_t *>(pVideoFrame));
 }
 
 JNIEXPORT void JNICALL Java_me_walkerknapp_devolay_DevolayReceiver_freeAudioV2(JNIEnv *env, jclass jClazz, jlong pReceiver, jlong pAudioFrame) {
-    getNDILib()->recv_free_audio_v2(reinterpret_cast<NDIlib_recv_instance_t *>(pReceiver),
+    getNDILib()->recv_free_audio_v2(reinterpret_cast<NDIlib_recv_instance_t>(pReceiver),
                               reinterpret_cast<NDIlib_audio_frame_v2_t *>(pAudioFrame));
 }
 
 JNIEXPORT void JNICALL Java_me_walkerknapp_devolay_DevolayReceiver_freeMetadata(JNIEnv *env, jclass jClazz, jlong pReceiver, jlong pMetadataFrame) {
-    getNDILib()->recv_free_metadata(reinterpret_cast<NDIlib_recv_instance_t *>(pReceiver),
+    getNDILib()->recv_free_metadata(reinterpret_cast<NDIlib_recv_instance_t>(pReceiver),
                               reinterpret_cast<NDIlib_metadata_frame_t *>(pMetadataFrame));
 }
 
 JNIEXPORT jboolean JNICALL Java_me_walkerknapp_devolay_DevolayReceiver_receiveSendMetadata(JNIEnv *env, jclass jClazz, jlong pReceiver, jlong pMetadataFrame) {
-    return getNDILib()->recv_send_metadata(reinterpret_cast<NDIlib_recv_instance_t *>(pReceiver),
+    return getNDILib()->recv_send_metadata(reinterpret_cast<NDIlib_recv_instance_t>(pReceiver),
                                      reinterpret_cast<NDIlib_metadata_frame_t *>(pMetadataFrame));
 }
 
 JNIEXPORT void JNICALL Java_me_walkerknapp_devolay_DevolayReceiver_receiveGetPerformance(JNIEnv *env, jclass jClazz, jlong pReceiver, jlong pTotalPerformance, jlong pDroppedPerformance) {
-    getNDILib()->recv_get_performance(reinterpret_cast<NDIlib_recv_instance_t *>(pReceiver),
+    getNDILib()->recv_get_performance(reinterpret_cast<NDIlib_recv_instance_t>(pReceiver),
                                 reinterpret_cast<NDIlib_recv_performance_t *>(pTotalPerformance),
                                 reinterpret_cast<NDIlib_recv_performance_t *>(pDroppedPerformance));
 }
 
 JNIEXPORT void JNICALL Java_me_walkerknapp_devolay_DevolayReceiver_receiveClearConnectionMetadata(JNIEnv *env, jclass jClazz, jlong pReceiver) {
-    getNDILib()->recv_clear_connection_metadata(reinterpret_cast<NDIlib_recv_instance_t *>(pReceiver));
+    getNDILib()->recv_clear_connection_metadata(reinterpret_cast<NDIlib_recv_instance_t>(pReceiver));
 }
 
 JNIEXPORT void JNICALL Java_me_walkerknapp_devolay_DevolayReceiver_receiveAddConnectionMetadata(JNIEnv *env, jclass jClazz, jlong pReceiver, jlong pMetadataFrame) {
-    getNDILib()->recv_add_connection_metadata(reinterpret_cast<NDIlib_recv_instance_t *>(pReceiver),
+    getNDILib()->recv_add_connection_metadata(reinterpret_cast<NDIlib_recv_instance_t>(pReceiver),
                                         reinterpret_cast<NDIlib_metadata_frame_t *>(pMetadataFrame));
 }
 
 JNIEXPORT jint JNICALL Java_me_walkerknapp_devolay_DevolayReceiver_receiveGetNoConnections(JNIEnv *env, jclass jClazz, jlong pReceiver) {
-    return getNDILib()->recv_get_no_connections(reinterpret_cast<NDIlib_recv_instance_t *>(pReceiver));
+    return getNDILib()->recv_get_no_connections(reinterpret_cast<NDIlib_recv_instance_t>(pReceiver));
 }
 
 JNIEXPORT jstring JNICALL Java_me_walkerknapp_devolay_DevolayReceiver_receiveGetWebControl(JNIEnv *env, jclass jClazz, jlong pReceiver) {
-    const char *webControl = getNDILib()->recv_get_web_control(reinterpret_cast<NDIlib_recv_instance_t *>(pReceiver));
+    const char *webControl = getNDILib()->recv_get_web_control(reinterpret_cast<NDIlib_recv_instance_t>(pReceiver));
     auto *ret = env->NewStringUTF(webControl);
-    getNDILib()->recv_free_string(reinterpret_cast<NDIlib_recv_instance_t *>(pReceiver), webControl);
+    getNDILib()->recv_free_string(reinterpret_cast<NDIlib_recv_instance_t>(pReceiver), webControl);
     return ret;
 }
